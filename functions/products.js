36be19b4e5abd18e3205d7f06510b33dbe70bab2
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-01-18 16:25:16 +0100

    initial commit

---
+++ functions/products.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+
+const Airtable = require("airtable-node");
+const airtable = new Airtable({
+  apiKey: process.env.AIRTABLE_API,
+})
+  .base("appzrEOwuBAF4B6ab")
+  .table("products_ecom");
+exports.handler = async (event, context) => {
+  try {
+    const { records } = await airtable.list();
+    const products = records.map((p) => {
+      const {
+        id,
+        fields: {
+          name,
+          image,
+          description,
+          company,
+          category,
+          colors,
+          shipping,
+          price,
+          featured,
+        },
+      } = p;
+      const url = image[0].url;
+      return {
+        id,
+        name,
+        description,
+        company,
+        category,
+        colors,
+        shipping,
+        price,
+        featured,
+        url,
+      };
+    });
+    return {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 200,
+      body: JSON.stringify(products),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: "server error",
+    };
+  }
+};
